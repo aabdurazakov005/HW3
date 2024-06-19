9c1d4828a5e437b1d4668a8db06861e5e7366223
aabdurazakov005 <aabdurazakov005@yandex.ru>
2024-06-19 18:47:31 +0300

    третья ДЗ.

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        byte a = 1;
+        short b = 3367;
+        int c = 2341775;
+        long d = 12324353476576L;
+        float e = 3.47f;
+        double f = 4543.43256;
+        System.out.println("Задание №1");
+        System.out.println("Значение переменной а с типом byte равно " + a);
+        System.out.println("Значение переменной b с типо short равно " + b);
+        System.out.println("Значение переменной с с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной е с типом float равно " + e);
+        System.out.println("Значение переменной f с типои double равно " + f);
+        System.out.println(" ");
+        float firstNumber = 27.12f;
+        long secondNumber = 987678965549L;
+        float thirdNumber = 2.786f;
+        short fourthNumber = 569;
+        short fifthNumber = -159;
+        short sixthNumber = 27897;
+        byte seventhNumber = 67;
+        byte classLP = 23;
+        byte classAS = 27;
+        byte classEA = 30;
+        short paperSheets = 480;
+        int totalStudents = classLP + classAS + classEA;
+        int sheetOnEach = paperSheets / totalStudents;
+        System.out.println("Задание №3");
+        System.out.println("На каждого ученика рассчитано " + sheetOnEach + " листов бумаги");
+
+
+
+
+    }
+}
